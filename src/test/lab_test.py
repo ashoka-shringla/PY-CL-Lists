60febd12419d4bddcc894cc80917a3a015f80648
Kaveri Priya <kaveri.priya@revature.com>
2024-02-29 02:28:16 +0530

    Test added

--- src/test/lab_test.py
+++ src/test/lab_test.py
@@ -37,5 +37,5 @@ class TestLabFunctions(unittest.TestCase):
         self.assertEqual(combine_lists(list1, list2), [1, 2, 3, 4, 5])
 
 
-if __name__ == '__main__':
+if __name__ == '__main__': 
     unittest.main()
